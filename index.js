d53e9ddaa195d2f1b79ca63ab6d6a829fb21d329
Kolade Afode <koladeafode@Kolades-iMac.local>
2021-11-29 08:07:56 +0100

    Complete exercise 3.9

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 morgan.token("json", function (req, res) {
   return JSON.stringify(req.body);
