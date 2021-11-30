7f91289dfb60416b24c4ae73cfbc72acd12ee191
Kolade Afode <kolade.afode@yahoo.com>
2021-11-30 10:43:29 +0100

    Complete exercises 3.20-3.21

--- index.js
+++ index.js
@@ -63,12 +63,8 @@ app.put("/api/persons/:id", (request, response, next) => {
     number: body.number,
   };
 
-  Person.findByIdAndUpdate(
-    request.params.id,
-    person,
-    { new: true },
-    { runValidators: true }
-  )
+  const opts = { runValidators: true };
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
