e6e7154b3cb21a75aa01ed61ecc1bf9aad7ba1a1
Kolade Afode <kolade.afode@yahoo.com>
2021-11-30 10:13:08 +0100

    Complete exercises 3.19-3.20

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
@@ -61,7 +63,12 @@ app.put("/api/persons/:id", (request, response, next) => {
     number: body.number,
   };
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    person,
+    { new: true },
+    { runValidators: true }
+  )
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
@@ -76,7 +83,7 @@ app.delete("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
   if (body.name === undefined) {
     return response.status(404).json({
@@ -96,9 +103,12 @@ app.post("/api/persons", (request, response) => {
     date: new Date(),
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const url = process.env.MONGODB_URI;
 
@@ -12,8 +13,12 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: { type: String, required: true, unique: true, minLength: 3 },
+  number: { type: String, required: true, minLength: 8 },
+});
+
+personSchema.plugin(uniqueValidator, {
+  message: "Error, expected {PATH} to be unique.",
 });
 
 personSchema.set("toJSON", {
